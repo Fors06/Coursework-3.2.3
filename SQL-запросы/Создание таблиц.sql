cb62078f0379f46cb75dc187be6d5ad9783ee0e6
Fors06 <iv_artem06@mail.ru>
2025-06-20 23:09:31 +0300

    Добавил запросы SQL
    И отредактировал создание таблиц (убрал лишние комментарии)

--- SQL-запросы/Создание таблиц.sql
+++ SQL-запросы/Создание таблиц.sql
@@ -12,45 +12,12 @@ CREATE TABLE Пользователь (
     Имя VARCHAR(100) NOT NULL,
     Фамилия VARCHAR(50) NOT NULL,
 	Отчество VARCHAR(50),
-    Email VARCHAR(100) NOT NULL UNIQUE,
+    Email VARCHAR(100),
     Пароль VARCHAR(255) NOT NULL,
-    Телефон VARCHAR(20),
+    Телефон VARCHAR(20) NOT NULL,
     User_Type_id INT NOT NULL,
     FOREIGN KEY (User_Type_id) REFERENCES [Тип Пользователя](Id)
 );
-
--- Статусы карты
---CREATE TABLE [Статус карты] (
-  --  Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-   -- Status VARCHAR(20)
---);
-
--- Состояния карты
---CREATE TABLE [Состояние карты] (
- --   Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-  --  State VARCHAR(20)
---);
-
--- Карта лояльности
---CREATE TABLE [Карта лояльности] (
-  --  Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-   -- Баллы INT NOT NULL,
-    --Номер_Карты BIGINT NOT NULL,
-    --Card_Status INT NOT NULL,
-    --Card_State INT NOT NULL,
-    --Phone VARCHAR(20) NOT NULL,
-    --FOREIGN KEY (Card_Status) REFERENCES [Статус карты](Id),
-    --FOREIGN KEY (Card_State) REFERENCES [Состояние карты](Id)
---);
-
--- Программа лояльности
---CREATE TABLE [Программа лояльности] (
-  --  Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-   -- Название VARCHAR(100) NOT NULL,
-   -- [Условия участия] VARCHAR(100) NOT NULL,
-   -- Описание TEXT NOT NULL,
-   -- Скидка DECIMAL(5, 2)
---);
 
 -- Клиенты
 CREATE TABLE Клиент (
@@ -60,11 +27,7 @@ CREATE TABLE Клиент (
     Отчество VARCHAR(50),
     Телефон VARCHAR(20),
     Users_id INT NOT NULL,
-   -- Program_id INT,
-  --  Card_id INT,
     FOREIGN KEY (Users_id) REFERENCES Пользователь(Id),
-  --  FOREIGN KEY (Program_id) REFERENCES [Программа лояльности](Id),
-  --  FOREIGN KEY (Card_id) REFERENCES [Карта лояльности](Id)
 );
 
 -- Автопредприятие
@@ -82,9 +45,6 @@ CREATE TABLE Автосервисная (
 CREATE TABLE Неисправность (
     Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
     Описание TEXT,
-   -- Cars_id INT NOT NULL,
-    --FOREIGN KEY (Cars_id) REFERENCES Автомобиль(Id), --ON DELETE CASCADE
-
 );
 
 -- Автомобили
@@ -96,7 +56,7 @@ CREATE TABLE Автомобиль (
     Client_id INT NOT NULL,
 	Malfunction_id int NOT NULL,
     FOREIGN KEY (Client_id) REFERENCES Клиент(Id),
-	FOREIGN KEY (Malfunction_id) REFERENCES Автосервисная(Id)
+	FOREIGN KEY (Malfunction_id) REFERENCES Неисправность(Id)
 );
 
 -- Услуги
@@ -131,18 +91,16 @@ CREATE TABLE [Статус заказа] (
     Статус VARCHAR(20) NOT NULL
 );
 
+--Заказ
 CREATE TABLE Заказ (
     Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-    Дата_начала DATETime NOT NULL,
-	Дата_окончания DateTime NOT NULL,
-    Стоимость DECIMAL(10, 2) NOT NULL,
     Cars_id int NOT NULL,
     Clients_id INT,
     Master_id INT NOT NULL,
     Auto_Service_id INT NOT NULL,
     Statuses_id INT NOT NULL,
     Services_id INT NOT NULL,
-    FOREIGN KEY (Cars_id) REFERENCES Автомобиль(Id), --ON DELETE CASCADE,
+    FOREIGN KEY (Cars_id) REFERENCES Автомобиль(Id),
     FOREIGN KEY (Clients_id) REFERENCES Клиент(Id),
     FOREIGN KEY (Master_id) REFERENCES Сотрудник(Id),
     FOREIGN KEY (Auto_Service_id) REFERENCES Автосервисная(Id),
@@ -150,17 +108,3 @@ CREATE TABLE Заказ (
     FOREIGN KEY (Services_id) REFERENCES Услуги(Id)
 );
 
-
-
-
-
--- Удаляю прежнюю связь между заказом и услугами
---ALTER TABLE Заказ DROP COLUMN Services_id;
-
---CREATE TABLE Заказ_услуга (
-  --  Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-    --Order_id INT NOT NULL,
-   -- Services_id INT NOT NULL,
-    --FOREIGN KEY (Order_id) REFERENCES Заказ(Id),
-   -- FOREIGN KEY (Services_id) REFERENCES Услуги(Id)
---);
